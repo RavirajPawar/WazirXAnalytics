464de1e97c1f619d224e5dc419d85f072bc89543
Raviraj <pawarraviraj9160@gmail.com>
2021-12-07 23:33:01 +0530

    configuration files

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas as pd
 from pymongo import MongoClient
 
@@ -20,25 +19,33 @@ REQUIRED_SHEETS = ['Additional Transfers',
                    'Exchange Trades',
                    'Deposits and Withdrawals'
                    ]
+COLLECTION_NAMES = ["additional_transfers",
+                    "exchange_trades",
+                    "deposits_and_withdrawals"]
 
 connection = MongoClient()
-# deposites_and_withdrawals | exchange_trades | additional_transfers
+
 db = connection["wazirx_analytics"]
 counter = 0
 
 """
 prototype 1 for inserting data in dbs
+additional_transfers 874 additional_transfers
+exchange_trades 80
+deposits_and_withdrawals 8
+
 """
 
-for file in os.listdir(EXCEL_FILE_PATH)[:-3]:
+for file in os.listdir(EXCEL_FILE_PATH)[:]:
     file_path = os.path.join(EXCEL_FILE_PATH, file)
-    df = pd.read_excel(file_path, sheet_name=REQUIRED_SHEETS[0])
-    print(file)
-    for _, row in df.iterrows():
-        counter += 1
-        db["additional_transfers"].update(
-            dict(row), dict(row), upsert=True)
+    for sheet, collection in zip(REQUIRED_SHEETS, COLLECTION_NAMES):
+        df = pd.read_excel(file_path, sheet_name=sheet)
+        print(file)
+        for _, row in df.iterrows():
+            counter += 1
+            db[collection].update(
+                dict(row), dict(row), upsert=True)
 
-    print("-" * 80)
+        print("-" * 80)
 
 print("counter ", counter)
