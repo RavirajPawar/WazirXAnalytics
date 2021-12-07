87823a24a06559d51376402ecf7cb7f8a831b202
Raviraj <pawarraviraj9160@gmail.com>
2021-12-07 23:33:46 +0530

    coin and overall ROI feature developed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import requests
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import requests
 
 from constants import BUY_KEYS, SELL_KEYS, URL
 from utility import total_portfolio, coin_analyzer, get_referral_data, get_total_deposits_and_withdrawals
@@ -19,13 +19,22 @@ def portfolio(market=None):
         coin_data, buy_trades, sell_trades = total_portfolio(coin_specific_trades)
         coin_data = coin_analyzer(coin_data[market])
         wazirx_coin_data = requests.get(URL.format(market.lower())).json()["ticker"]
-        return_on_investment = (float(wazirx_coin_data["sell"])*100)/ coin_data["avg_buy_price"]
-        return_on_investment = return_on_investment - 100
+        wazirx_coin_data["coin_roi"] = ((float(wazirx_coin_data["sell"]) * 100) / coin_data["avg_buy_price"]) - 100
+
+        if (coin_data["final_investment_on_buy"] - coin_data["final_sell_earning"]) > 0:
+            wazirx_coin_data["overall_roi"] = (float(wazirx_coin_data["sell"]) * coin_data["coin_balance"] * 100) / \
+                                              (coin_data["final_investment_on_buy"] - coin_data[
+                                                  "final_sell_earning"]) - 100
+        else:
+            wazirx_coin_data["overall_roi"] = (((float(wazirx_coin_data["sell"]) * coin_data["coin_balance"]) + \
+                                                coin_data["final_sell_earning"]) * 100) / \
+                                              coin_data["final_investment_on_buy"]
+
         return render_template("specific_market.html", coin_data=coin_data, market=market,
                                BUY_KEYS=BUY_KEYS, SELL_KEYS=SELL_KEYS,
                                buy_trades=buy_trades, sell_trades=sell_trades,
-                               wazirx_coin_data = wazirx_coin_data,
-                               return_on_investment= return_on_investment)
+                               wazirx_coin_data=wazirx_coin_data
+                               )
     else:
         all_trades = mongo.db.exchange_trades.find({}, {'_id': False})
         my_trades, _, _ = total_portfolio(all_trades)
@@ -38,8 +47,8 @@ def index():
     referral_earnings = get_referral_data(referral_data)
     deposits_and_withdrawals_data = mongo.db.deposits_and_withdrawals.find({}, {'_id': False})
     total_deposits_and_withdrawals = get_total_deposits_and_withdrawals(deposits_and_withdrawals_data)
-    return render_template("home.html", referral_earnings= referral_earnings,
-                           total_deposits_and_withdrawals = total_deposits_and_withdrawals)
+    return render_template("home.html", referral_earnings=referral_earnings,
+                           total_deposits_and_withdrawals=total_deposits_and_withdrawals)
 
 
 if __name__ == "__main__":
--- utility.py
+++ utility.py
@@ -105,9 +105,19 @@ def get_total_deposits_and_withdrawals(deposits_and_withdrawals_data):
     total_deposits_and_withdrawals = dict()
     for record in deposits_and_withdrawals_data:
         if record["Currency"] in total_deposits_and_withdrawals:
-            total_deposits_and_withdrawals[record["Currency"]] += record["Volume"]
+            if record["Transaction"] == "Deposit":
+                total_deposits_and_withdrawals[record["Currency"]]["Deposit"] += record["Volume"]
+            else:
+                total_deposits_and_withdrawals[record["Currency"]]["Withdrawal"] += record["Volume"]
         else:
-            total_deposits_and_withdrawals[record["Currency"]] = record["Volume"]
+            total_deposits_and_withdrawals[record["Currency"]] = dict()
+            if record["Transaction"] == "Deposit":
+                total_deposits_and_withdrawals[record["Currency"]]["Deposit"] = record["Volume"]
+                total_deposits_and_withdrawals[record["Currency"]]["Withdrawal"] = 0
+            else:
+                total_deposits_and_withdrawals[record["Currency"]]["Deposit"] = 0
+                total_deposits_and_withdrawals[record["Currency"]]["Withdrawal"] = record["Volume"]
+
 
     return total_deposits_and_withdrawals
 
